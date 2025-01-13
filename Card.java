53de63d0e9f0b1fcb222ba2401244e773e08a312
MartiniumOxide <ypeng25@sandomenico.org>
2025-01-12 16:10:13 -0800

    1

--- Card.java
+++ Card.java
@@ -9,6 +9,70 @@ import greenfoot.*;
 
 public class Card extends Actor
 {
+   public enum Shape
+   {
+       CIRCLE, SQUARE, TRIANGLE, NO_SHAPE;
+   }
+   public enum Color
+   {
+       RED, GREEN, BLUE, NO_COLOR;
+   }
+   private Shape shape;
+   private Color color;
+   private boolean isSelected;
+   private GreenfootImage cardImage, selectedCardImage;
+   private int numberOfShapes, shading;
    
+   public Card(Shape shape, Color color, int numberOfShapes, 
+   int shading, GreenfootImage cardImage, GreenfootImage selectedCardImage)
+   {
+       this.shape = shape;
+       this.color = color;
+       this.numberOfShapes = numberOfShapes;
+       this.shading = shading;
+       this.cardImage = cardImage;
+       this.selectedCardImage = selectedCardImage;
+       setImage(cardImage);
+   }
+   
+   public Shape getShape()
+   {
+       return shape;
+   }
+   
+   public Color getColor()
+   {
+       return color;
+   }
+   
+   public boolean getIsSelected()
+   {
+       return isSelected;
+   }
+   
+   public GreenfootImage getCardImage()
+   {
+       return cardImage;
+   }
+   
+   public GreenfootImage getSelectedCardImage()
+   {
+       return selectedCardImage;
+   }
+   
+   public int getNumberOfShapes()
+   {
+       return numberOfShapes;
+   }
+   
+   public int getShading()
+   {
+       return shading;
+   }
+   
+   public void setIsSelected(boolean isSelected)
+   {
+       this.isSelected = isSelected;
+   }
 }
 
