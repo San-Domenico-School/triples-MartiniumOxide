56ee5df05c314b7f92c193e92073fc655b69cd3a
MartiniumOxide <ypeng25@sandomenico.org>
2025-01-28 21:57:08 -0800

    1

--- Dealer.java
+++ Dealer.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.ArrayList;
 /**
- * Write a description of class Dealer here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * deal the cards
+ * check if triples
+ * @martin
+ * @Jan 28, 2025
  */
 public class Dealer extends Actor
 {
@@ -14,7 +14,10 @@ public class Dealer extends Actor
     private Card[] cardsSelected;
     private int numCardsInDeck;
     private int triplesRemaining;
-    
+    private boolean shapeFactor;
+    private boolean colorFactor;
+    private boolean numberOfShapesFactor;
+    private boolean shadingFactor;
     public Dealer(int numCardsInDeck) 
     {
         this.numCardsInDeck = numCardsInDeck;
@@ -40,7 +43,7 @@ public class Dealer extends Actor
             for (int j = 0; j < 3; j++)
             {
                 Card card = deck.getTopCard();
-                getWorld().addObject(card, 100*i, 100*j);
+                getWorld().addObject(card, 130*j+80, 90*i+50);
             }
         }
     }
@@ -50,9 +53,8 @@ public class Dealer extends Actor
     {
         int cardsRemaining = deck.getNumCardsInDeck();
         Integer score = triplesRemaining;
-        //example text
-        getWorld().showText("Cards Remaining: " + cardsRemaining, 50, 50);
-        getWorld().showText("Score: " + score, 50, 80);
+        getWorld().showText("" + cardsRemaining, 314, 469);
+        getWorld().showText("" + score, 314, 505);
     }
 
     
@@ -61,10 +63,102 @@ public class Dealer extends Actor
         Greenfoot.stop();
     }
 
-    public void checkIfTriple() {
-        // Logic to check if selected cards form a triple
+    public void checkIfTriple(Card[] cardsSelected) 
+    {
+        this.cardsSelected = cardsSelected;
+        checkShape(cardsSelected);
+        checkColor(cardsSelected);
+        checkNumberOfShapes(cardsSelected);
+        checkShading(cardsSelected);
+        if (shapeFactor && colorFactor && numberOfShapesFactor
+            && shadingFactor)
+        {
+            actionIfTriple();
+            System.out.print("triple found");
+        }
+    }
+    
+    public boolean checkShape(Card[] cardsSelected)
+    {
+        if (cardsSelected[0].getShape() == cardsSelected[1].getShape() 
+            && cardsSelected[0].getShape() == cardsSelected[2].getShape() )
+        {
+            shapeFactor = true;
+        }
+        else if (cardsSelected[0].getShape() != cardsSelected[1].getShape() 
+            && cardsSelected[0].getShape() != cardsSelected[2].getShape() 
+            && cardsSelected[1].getShape() != cardsSelected[2].getShape())
+        {
+            shapeFactor = true;
+        }
+        else
+        {
+            shapeFactor = false;
+        }
+        return shapeFactor;
+    }
+    
+    public boolean checkColor(Card[] cardsSelected)
+    {
+        if (cardsSelected[0].getColor() == cardsSelected[1].getColor() 
+            && cardsSelected[0].getColor() == cardsSelected[2].getColor() )
+        {
+            colorFactor = true;
+        }
+        else if (cardsSelected[0].getColor() != cardsSelected[1].getColor() 
+            && cardsSelected[0].getColor() != cardsSelected[2].getColor() 
+            && cardsSelected[1].getColor() != cardsSelected[2].getColor())
+        {
+            colorFactor = true;
+        }
+        else
+        {
+            colorFactor = false;
+        }
+        return colorFactor;
+    }
+    
+    public boolean checkNumberOfShapes(Card[] cardsSelected)
+    {
+        if (cardsSelected[0].getNumberOfShapes() == cardsSelected[1].getNumberOfShapes() 
+            && cardsSelected[0].getNumberOfShapes() == cardsSelected[2].getNumberOfShapes() )
+        {
+            numberOfShapesFactor = true;
+        }
+        else if (cardsSelected[0].getNumberOfShapes() != cardsSelected[1].getNumberOfShapes() 
+            && cardsSelected[0].getNumberOfShapes() != cardsSelected[2].getNumberOfShapes() 
+            && cardsSelected[1].getNumberOfShapes() != cardsSelected[2].getNumberOfShapes())
+        {
+            numberOfShapesFactor = true;
+        }
+        else
+        {
+            numberOfShapesFactor = false;
+        }
+        return numberOfShapesFactor;
     }
 
+    public boolean checkShading(Card[] cardsSelected)
+    {
+        if (cardsSelected[0].getShading() == cardsSelected[1].getShading() 
+            && cardsSelected[0].getShading() == cardsSelected[2].getShading() )
+        {
+            shadingFactor = true;
+        }
+        else if (cardsSelected[0].getShading() != cardsSelected[1].getShading() 
+            && cardsSelected[0].getShading() != cardsSelected[2].getShading() 
+            && cardsSelected[1].getShading() != cardsSelected[2].getShading())
+        {
+            shadingFactor = true;
+        }
+        else
+        {
+            shadingFactor = false;
+        }
+        return shadingFactor;
+    }
+
+    
     public void actionIfTriple() {
         // Logic for actions if a triple is found
     }
